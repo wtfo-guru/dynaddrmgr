2e09e673de9250b6d4a2c6b83ce7ce0cd5fb84c7
Quien Sabe <qs5779@mail.com>
2024-04-28 13:21:16 -0400

    poetry update (#108)
    * chore: poetry update, optonal test, mypy strict false
    
    * fix: object base class usage
    
    * chore: poetry update
    
    * chore: bump version
    
    * fix: Corrected load config file to use base for multiple configs
    
    * chore: poetry update
    
    * chore: poetry update
    
    ---------
    
    Co-authored-by: Quien Sabe <qs5579@mail.com>

--- dynaddrmgr/foos.py
+++ dynaddrmgr/foos.py
@@ -12,14 +12,14 @@ from wtforglib.files import load_yaml_file
 from wtforglib.kinds import StrAnyDict
 
 
-def load_config_file(config: str, debug: bool) -> StrAnyDict:
+def load_config_file(config: str, base: str, debug: bool) -> StrAnyDict:
     """Load the configuration file."""
     if not config:
-        config_path = Path.home() / ".config" / "dynaddrmgr.yaml"
+        config_path = Path.home() / ".config" / "{0}.yaml".format(base)
         if config_path.is_file():
             config = str(config_path)
         else:
-            config = "/etc/dynaddrmgr.yaml"
+            config = "/etc/{0}.yaml".format(base)
     if debug:
         click.echo("DEBUG: config file => {0}".format(config))
     return load_yaml_file(config, missing_ok=False)
